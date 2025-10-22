5fb71eddfef335c1c3a5ab409e690ab14ff40ca4
turnben347 <turneben@mail.gvsu.edu>
2025-10-21 20:53:12 -0400

    Add conditional rendering for beverage layers

--- src/components/Beverage.vue
+++ src/components/Beverage.vue
@@ -1,20 +1,28 @@
 <template>
   <Mug>
+    <!-- Temperature layer -->
     <Cold v-if="isIced" />
     <Hot v-else />
+
     <Contents>
+      <!-- Creamer layer only if selected -->
       <template v-slot:top>
-        <Creamer />
+        <Creamer v-if="creamer !== 'No Creamer'" :creamer="creamer" />
       </template>
+
+      <!-- Syrup layer only if selected -->
       <template v-slot:mid>
-        <Syrup />
+        <Syrup v-if="syrup !== 'No Syrup'" :syrup="syrup" />
       </template>
+
+      <!-- Base beverage is always shown -->
       <template v-slot:bottom>
-        <Base />
+        <Base :base="base" />
       </template>
     </Contents>
   </Mug>
 </template>
+
 <script setup lang="ts">
 import Contents from "./Contents.vue";
 import Mug from "./Mug.vue";
@@ -26,6 +34,10 @@ import Cold from "./Cold.vue";
 
 type Props = {
   isIced: boolean;
+  base: string;
+  creamer: string;
+  syrup: string;
 };
+
 defineProps<Props>();
 </script>
